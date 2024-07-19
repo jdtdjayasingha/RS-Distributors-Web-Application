9b0a22623b1a67c810c86342e0b62fcdd35255ab
Tharindu Dilshan <153268657+jdtdjayasingha@users.noreply.github.com>
2024-07-19 16:34:40 +0530

    Created the file structure

---
+++ Pages/Admin/Product.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RsDistributors.Pages.Admin
+{
+    public class ProductModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Pages/Seller/Delmo/DelmoPage2.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RsDistributors.Pages.Seller.Delmo
+{
+    public class DelmoPage2Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
