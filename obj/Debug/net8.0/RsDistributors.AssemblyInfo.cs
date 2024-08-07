09f8555b9a7a073e5d2b29800cd57c174f7f129f
Tharindu Dilshan <jdtdjayasingha@gmail.com>
2024-08-07 23:23:49 +0530

    Created Loging Text fields

--- obj/Debug/net8.0/RsDistributors.AssemblyInfo.cs
+++ obj/Debug/net8.0/RsDistributors.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RsDistributors")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2715843ad11c7b4c1d5e6363284f1c6c8cf049b7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01560d3936c70dded98041e9ac100ac0d9ea27e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("RsDistributors")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RsDistributors")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
