e95846f0e0adc71c9fe174fd24904d2c0076f91a
Tharindu Dilshan <jdtdjayasingha@gmail.com>
2024-08-12 23:36:55 +0530

    Created _AdminLayout.cshtml

--- obj/Debug/net8.0/RsDistributors.AssemblyInfo.cs
+++ obj/Debug/net8.0/RsDistributors.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RsDistributors")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5df84f0a76754105647b8bb7cd00a1cd891c7f8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ad5675fcf2fb3a12fda01824c9e5e935066f02e")]
 [assembly: System.Reflection.AssemblyProductAttribute("RsDistributors")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RsDistributors")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
