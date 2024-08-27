35c268987253e4584f66d15f6ac816e36b600872
Tharindu Dilshan <jdtdjayasingha@gmail.com>
2024-08-27 12:38:33 +0530

    Created user authentication

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -24,6 +24,54 @@ namespace RsDistributors.Pages
         {
 
         }
+
+        public IActionResult OnPost()
+        {
+            using (SqlConnection con = new SqlConnection(_connectionString))
+            {
+                con.Open();
+                string query = "SELECT * FROM Users WHERE Email=@Email AND Password=@Password";
+                SqlCommand cmd = new SqlCommand(query, con);
+                cmd.Parameters.AddWithValue("@Email", emailLogin);
+                cmd.Parameters.AddWithValue("@Password", passwordLogin);
+
+                SqlDataAdapter sda = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                sda.Fill(dt);
+
+                if (dt.Rows.Count > 0)
+                {
+                    string passStatus = dt.Rows[0]["Category"]?.ToString() ?? string.Empty;
+
+                    if (passStatus == "besto")
+                    {
+                        return RedirectToPage("/Seller/Besto/BestoPage1");
+                    }
+                    else if (passStatus == "delmo")
+                    {
+                        return RedirectToPage("/Seller/Delmo/DelmoPage1");
+                    }
+                    else
+                    {
+                        infoMsg = "Invalid Role!";
+                    }
+                }
+                else
+                {
+                    if (emailLogin == "admin@gmail.com" && passwordLogin == "admin")
+                    {
+                        return RedirectToPage("Admin/Dashboard");
+                    }
+                    else
+                    {
+                        infoMsg = "Invalid username or password!";
+                    }
+                }
+            }
+
+            return Page();
+        }
+    }
 }
 
 
