678c6b639dec726025e17f1aa7e7dc5c26f6c31a
Tharindu Dilshan <jdtdjayasingha@gmail.com>
2024-08-28 18:08:35 +0530

    Created BestoPage1.cshtml.cs

--- Pages/Seller/Besto/BestoPage1.cshtml.cs
+++ Pages/Seller/Besto/BestoPage1.cshtml.cs
@@ -1,14 +1,75 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
 
-namespace RsDistributors.Pages.Seller.Besto
+namespace RsDistributors.View.Seller.Besto
 {
     public class BestoPage1Model : PageModel
     {
-        public void OnGet()
+        private readonly string? _connectionString;
+        public BestoPage1Model(IConfiguration configuration)
         {
+            _connectionString = configuration.GetConnectionString("DefaultConnection");
         }
 
-        public required string DropDownShop { get; set; }
+        public List<SelectListItem> Shops { get; set; } = new List<SelectListItem>();
+        [BindProperty]
+        public required string SelectedShop { get; set; }
+        public required string ErrorMessage { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            await LoadShopsAsync();
+        }
+
+        public async Task<IActionResult> OnPostNextAsync()
+        {
+            try
+            {
+                await DelBillAsync();
+                
+                return RedirectToPage("BestoPage2");
+            }
+            catch (Exception ex)
+            {
+                ErrorMessage = ex.Message;
+                return RedirectToPage("BestoPage2");
+            }
+        }
+
+        private async Task LoadShopsAsync()
+        {
+            using (var con = new SqlConnection(_connectionString))
+            {
+                await con.OpenAsync();
+                using (var cmd = new SqlCommand("SELECT ID, Name FROM ShopTB WHERE Category='Besto'", con))
+                {
+                    var reader = await cmd.ExecuteReaderAsync();
+                    while (await reader.ReadAsync())
+                    {
+                        Shops.Add(new SelectListItem
+                        {
+                            Value = reader["ID"].ToString(),
+                            Text = reader["Name"].ToString()
+                        });
+                    }
+                }
+            }
+        }
+
+        private async Task DelBillAsync()
+        {
+            using (var con = new SqlConnection(_connectionString))
+            {
+                await con.OpenAsync();
+                using (var cmd = new SqlCommand("DELETE FROM BillTB", con))
+                {
+                    await cmd.ExecuteNonQueryAsync();
+                }
+            }
+        }
     }
 }
